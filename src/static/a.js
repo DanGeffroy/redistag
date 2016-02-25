ed1d7df5b4fddc0fa93c653cb3786e3f0aca1c75
Dan Geffroy <dan.geffroy@etu.univ-nantes.fr>
2016-02-25 14:55:44 +0100

    adding index page for links view

--- src/static/a.js
+++ src/static/a.js
@@ -1,3 +1,11 @@
 module.exports = function(){
   console.log('hello');
+
+  $.getJSON( "http://localhost:8080/lists", function( json ) {
+  json.data.forEach(function(entry) {
+    console.log(entry.href);
+    $( "#listLinks" ).append( "  <li class='list-group-item'><a href='"+entry.href+"'>"+entry.href+"</a></li>" );
+});
+
+ });
 };
